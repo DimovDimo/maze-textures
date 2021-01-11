475abd7817b68e4bdd6c2dba5933d09437f9a69d
DimovDimo <d.dimov1996@gmail.com>
2021-01-11 10:49:01 +0200

    added bootstrap & button "New texture"

--- app/resources/js/maze-textures.js
+++ app/resources/js/maze-textures.js
@@ -1,2 +1,9 @@
-let canvas = document.getElementById("maze-texture");
-let texture = canvas.getContext("2d");
+let newTexture = document.getElementById("new-texture");
+newTexture.addEventListener("click", engine);
+
+engine();
+
+function engine() {
+	let canvas = document.getElementById("maze-texture");
+    let texture = canvas.getContext("2d");
+}
