97b06e974d5d7ff35934a91e90cfcc6d81d4b659
DimovDimo <d.dimov1996@gmail.com>
2021-01-12 12:26:34 +0200

    added Maze Columns & Rows

--- app/resources/js/maze-textures.js
+++ app/resources/js/maze-textures.js
@@ -5,5 +5,16 @@ engine();
 
 function engine() {
 	let canvas = document.getElementById("maze-texture");
-    let texture = canvas.getContext("2d");
+	let texture = canvas.getContext("2d");
+
+    let mazeColumns = getMazeDimension("maze-columns");
+    let mazeRows = getMazeDimension("maze-rows");
+}
+
+function getInputNumber(id) {
+	return Number(document.getElementById(id).value);
+}
+
+function getMazeDimension(id) {
+	return getInputNumber(id) + 1;
 }
